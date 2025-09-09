38bbe400d2ba92943572d495f42c5c3a58d2b775
nnkongh <haukong1308@gmail.com>
2025-09-09 22:55:55 +0700

    Register Service Adapter Layer

---
+++ Ecommerce.WebApi/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.WebApi.Interfaces;
+using Ecommerce.WebApi.Services;
+
+namespace Ecommerce.WebApi.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAdapterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICookieTokenService,CookieTokenService>();
+            return services;
+        }
+    }
+}
