c326d1599d1af196b8a6873b5ee296099aad4f1c
nnkongh <haukong1308@gmail.com>
2026-01-06 21:47:43 +0700

    Fixed Address

--- Ecommerce.Domain/Models/Address.cs
+++ Ecommerce.Domain/Models/Address.cs
@@ -8,39 +8,40 @@ namespace Ecommerce.Domain.Models
 {
     public class Address : IEquatable<Address>
     {
-        public string? District { get; set; }
-        public string? City { get; set; }
-        public string? PostalCode { get; set; }
-        public string? Ward { get; set; }
+        public string? District { get; private set; }
+        public string? City { get; private set; }
+        public string? Province { get; private set; }
+        public string? Ward { get; private set; }
+        public string? Street { get; private set; }
+
+        private Address() { }
         public bool Equals(Address? other)
         {
             if(other == null)
             {
                 return false;
             }
-            return District == other.District && City == other.City && PostalCode == other.PostalCode && Ward == other.Ward;
+            return District == other.District && City == other.City && Street == other.Street && Ward == other.Ward && Province == other.Province;
         }
-        public static Address Create(string? district, string? city, string postalcode, string? ward)
+        public static Address Create(string? district, string? city, string? province, string? street, string? ward)
         {
             var address = new Address()
             {
                 District = district,
                 City = city,
-                PostalCode = postalcode,
+                Street = street,
+                Province = province,
                 Ward = ward
             };
             return address;
         }
-        public static Address Update(string? district, string? city, string postalcode, string? ward)
+        public void Update(string? district, string? city, string? province, string? street, string? ward)
         {
-            var address = new Address()
-            {
-                District = district,
-                City = city,
-                PostalCode = postalcode,
-                Ward = ward
-            };
-            return address;
+            if(district != null) District = district;
+            if(city != null) City = city;
+            if(province != null) Province = province;
+            if(street != null) Street = street;
+            if(ward != null) Ward = ward;
         }
     }
 }
