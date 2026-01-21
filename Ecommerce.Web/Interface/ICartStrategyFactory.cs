e0e1c4b03eaf9f3bbf1d57623b61293383a0dc37
nnkongh <haukong1308@gmail.com>
2026-01-21 22:57:52 +0700

    Add Cart strategy interface

---
+++ Ecommerce.Web/Interface/ICartStrategyFactory.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.Web.Services.Strategy;
+
+namespace Ecommerce.Web.Interface
+{
+    public interface ICartStrategyFactory
+    {
+        ICartStrategy CreateCartStrategy(IHttpContextAccessor httpContext);
+    }
+}
