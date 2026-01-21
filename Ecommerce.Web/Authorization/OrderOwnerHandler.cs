0d2974338aeadfe431fa205db12d498058a4bb3e
nnkongh <haukong1308@gmail.com>
2026-01-21 23:01:53 +0700

    Add policy for order

---
+++ Ecommerce.Web/Authorization/OrderOwnerHandler.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Domain.Models;
+using Ecommerce.Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace Ecommerce.Web.Authorization
+{
+    public class OrderOwnerHandler : AuthorizationHandler<OrderOwnerRequirement,Order>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OrderOwnerRequirement requirement, Order order)
+        {
+            var userId = context.User.FindFirst(ClaimTypes.NameIdentifier);
+            if(userId == null)
+            {
+                return Task.CompletedTask;
+            }
+            if (order.CustomerId == userId.Value)
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask; ;
+        }
+    }
+}
---
+++ Ecommerce.Web/Authorization/OrderOwnerRequirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Ecommerce.Web.Authorization
+{
+    public class OrderOwnerRequirement : IAuthorizationRequirement
+    {
+    }
+}
