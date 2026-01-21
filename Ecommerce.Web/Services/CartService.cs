4af3918dce2c798b9db2f13e87b3aa7c7831f264
nnkongh <haukong1308@gmail.com>
2026-01-21 23:01:08 +0700

    Add Service cart

---
+++ Ecommerce.Web/Services/CartService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Ecommerce.Application.DTOs.Models;
+using Ecommerce.Application.DTOs.ModelsRequest.Cart;
+using Ecommerce.Application.Interfaces;
+using Ecommerce.Web.Interface;
+using Ecommerce.Web.Services.Strategy;
+using Ecommerce.Web.ViewModels;
+
+namespace Ecommerce.Web.Services
+{
+    public class CartService
+    {
+        private readonly ICartStrategyFactory _factory;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public CartService(ICartStrategyFactory factory, IHttpContextAccessor httpContext)
+        {
+            _factory = factory;
+            _httpContext = httpContext;
+        }
+        private ICartStrategy GetCartStrategy => _factory.CreateCartStrategy(_httpContext);
+        public Task AddToCartAsync(AddToCartRequest request) => GetCartStrategy.AddToCartAsync(request);
+        public Task ClearCartAsync() => GetCartStrategy.ClearCartAsync();
+        public Task<CartViewModel> GetCartAsync() => GetCartStrategy.GetCartAsync();
+        public Task RemoveFromCartAsync(int productId) => GetCartStrategy.RemoveFromCartAsync(productId);
+        public Task UpdateCartAsync(int productId, int quantity) => GetCartStrategy.UpdateCartAsync(productId, quantity);
+    }
+}
