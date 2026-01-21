2f31645103de3f53b8d56f817abd916dee8d3c5d
nnkongh <haukong1308@gmail.com>
2026-01-21 23:06:58 +0700

    Refactor specification pattern for 3 usecases

---
+++ Ecommerce.Domain/Specification/ProductWithCategorySpec.cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce.Domain.Models;
+using Ecommerce.Domain.Specification.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Specification
+{
+    public sealed class ProductWithCategorySpec : BaseSpecification<Product>
+    {
+        public ProductWithCategorySpec() : base() { }
+
+        public ProductWithCategorySpec(int parentCategoryId)
+            : base(p => p.Category.ParentId == parentCategoryId) 
+        { 
+            AddIncludes(p => p.Name);
+            AddIncludes(p => p.Category);
+        }
+
+        public ProductWithCategorySpec(string name) : base(p => p.Name.ToLower().Contains(name.ToLower())) 
+        {
+            AddIncludes(p => p.Category);
+        }
+        public ProductWithCategorySpec(int categoryId, bool isCategoryId) : base(p => p.CategoryId == categoryId || p.Category.ParentId == categoryId) 
+        {
+            AddOrderBy(p => p.Name);
+            AddIncludes(p => p.Category);
+        }
+
+    }
+
+}
