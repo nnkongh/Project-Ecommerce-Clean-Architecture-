79e6b9196918359066b3bae1b63e7a51400fb791
nnkongh <haukong1308@gmail.com>
2026-01-22 01:26:52 +0700

    Add get all product

---
+++ Ecommerce.Application/Common/Queries/Products/GetAllProducts/GetAllProductsHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Ecommerce.Application.DTOs.Models;
+using Ecommerce.Domain.Interfaces;
+using Ecommerce.Domain.Shared;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Common.Queries.Products.GetAllProducts
+{
+    internal class GetAllProductsHandler : IRequestHandler<GetAllProductsQuery, Result<IReadOnlyList<ProductModel>>>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllProductsHandler(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<IReadOnlyList<ProductModel>>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            var produtcs = await _productRepository.GetAsync();
+
+            var productModels = _mapper.Map<IReadOnlyList<ProductModel>>(produtcs);
+
+            return Result.Success(productModels);
+        }
+    }
+}
---
+++ Ecommerce.Application/Common/Queries/Products/GetAllProducts/GetAllProductsQuery.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Application.DTOs.Models;
+using Ecommerce.Domain.Shared;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Common.Queries.Products.GetAllProducts
+{
+    public sealed record GetAllProductsQuery : IRequest<Result<IReadOnlyList<ProductModel>>>
+    {
+    }
+}
