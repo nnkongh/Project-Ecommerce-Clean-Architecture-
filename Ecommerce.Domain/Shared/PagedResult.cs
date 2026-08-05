1a7303741162aab6ef9839d6a390fda4250f70d2
NguyenHau <haukong1308@gmail.com>
2026-08-05 18:59:17 +0700

    page result class

---
+++ Ecommerce.Domain/Shared/PagedResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Shared
+{
+    public class PagedResult<T>
+    {
+        public IReadOnlyList<T> Items { get; init; }
+        public int TotalItems { get; init; }
+        public int PageIndex { get; init; }   
+        public int PageSize { get; init; }
+
+        public int TotalPages => (int)Math.Ceiling(TotalItems / (double)PageSize);
+        public PagedResult() { }
+        public PagedResult(IReadOnlyList<T> items, int totalItems, int pageIndex, int pageSize)
+        {
+            Items = items;
+            TotalItems = totalItems;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+        }
+
+
+    }
+}
