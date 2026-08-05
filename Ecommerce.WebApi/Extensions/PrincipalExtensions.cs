5fdb54b02619c5d00a084b051c0255465f6d7190
NguyenHau <haukong1308@gmail.com>
2026-08-05 19:02:01 +0700

    Extension for getuserid by claimprincipal

---
+++ Ecommerce.WebApi/Extensions/PrincipalExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace Ecommerce.WebApi.Extensions
+{
+    public static class PrincipalExtensions
+    {
+        public static string? GetUserId(this ClaimsPrincipal principal)
+        {
+            var claim = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (claim != null)
+            {
+                return claim;
+            }
+            return null;
+
+        }
+    }
+}
