c6746d9ce9428015ceb5798e547283a80a642171
NguyenHau <haukong1308@gmail.com>
2026-08-05 18:51:29 +0700

    remove redudant using and configure unauthorize middleware

--- Ecommerce.Web/Program.cs
+++ Ecommerce.Web/Program.cs
@@ -1,32 +1,9 @@
-﻿using AutoMapper.Internal.Mappers;
-using Ecommerce.Application.Common.Command.Authentication.Login;
-using Ecommerce.Application.Common.Command.AuthenticationExternal;
-using Ecommerce.Application.Dependency;
-using Ecommerce.Application.Interfaces;
-using Ecommerce.Application.Mappers;
-using Ecommerce.Domain.Interfaces;
-using Ecommerce.Domain.Interfaces.UnitOfWork;
+﻿using Ecommerce.Application.Dependency;
 using Ecommerce.Infrastructure;
-using Ecommerce.Infrastructure.Data;
 using Ecommerce.Infrastructure.Dependency_Injection;
-using Ecommerce.Infrastructure.Identity;
-using Ecommerce.Infrastructure.Interfaces.Authentication;
 using Ecommerce.Infrastructure.Mapper;
-using Ecommerce.Infrastructure.Repository;
-using Ecommerce.Infrastructure.Repository.UnitOfWork;
-using Ecommerce.Infrastructure.Services.Authentication;
-using Ecommerce.Infrastructure.Services.Email;
 using Ecommerce.Web.Dependencies;
-using Ecommerce.Web.Interface;
-using Ecommerce.Web.Services;
-using Ecommerce.Web.ViewModels.ApiResponse;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OAuth;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
-using System.Runtime.Serialization;
-using System.Security.Principal;
+using Ecommerce.Web.Exceptions;
 
 namespace Ecommerce.Web
 {
@@ -52,17 +29,23 @@ namespace Ecommerce.Web
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseExceptionHandler(errorApp =>
             {
                 errorApp.Run(async context =>
                 {
+                    var exceptionHandlerPathFeature = context.Features.Get<Microsoft.AspNetCore.Diagnostics.IExceptionHandlerPathFeature>();
+                    if (exceptionHandlerPathFeature?.Error is UnauthorizedException)
+                    {
+                        context.Response.Redirect("/Auth/Login?expired=true");
+                        return;
+                    }
+
                     context.Response.ContentType = "application/json";
                     context.Response.StatusCode = 500;
-
-                    ApiResponse<string>.Fail("Failed");
+                    await context.Response.WriteAsJsonAsync(new { error = "Internal server error" });
                 });
             });
 
