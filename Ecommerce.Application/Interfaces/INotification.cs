9d357d28fbe4eddded9051cf4120388b5bbdd1ae
NguyenHau <haukong1308@gmail.com>
2026-08-11 16:11:52 +0700

    Add interface of notification signalr

---
+++ Ecommerce.Application/Interfaces/INotification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Interfaces
+{
+    public interface INotificationService
+    {
+        Task SendNotificationAsync(string userId, object message);
+    }
+}
