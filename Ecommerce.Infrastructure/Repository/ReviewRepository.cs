caff8c7f48a0e8b944b55af964ddf9aad462e2ef
NguyenHau <haukong1308@gmail.com>
2026-08-11 16:10:55 +0700

    Add Shop and review repository

---
+++ Ecommerce.Infrastructure/Repository/ReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Domain;
+using Ecommerce.Domain.Interfaces;
+using Ecommerce.Infrastructure.Data;
+using Ecommerce.Infrastructure.Repository.Base;
+
+namespace Ecommerce.Infrastructure.Repository
+{
+    public class ReviewRepository : GenericRepository<Review, int>, IReviewRepository
+    {
+        public ReviewRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Ecommerce.Infrastructure/Repository/ShopRepository.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Domain;
+using Ecommerce.Domain.Interfaces;
+using Ecommerce.Infrastructure.Data;
+using Ecommerce.Infrastructure.Repository.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Repository
+{
+    public class ShopRepository : GenericRepository<Shop, int>, IShopRepository
+    {
+        public ShopRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Shop?> GetByUserIdAsync(string userId)
+        {
+            return await _context.Shops.FirstOrDefaultAsync(s => s.UserId == userId);
+        }
+    }
+}
