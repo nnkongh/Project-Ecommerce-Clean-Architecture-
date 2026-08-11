edf4cfef63f71b5b9282a7bd905be0ec6fd805e5
NguyenHau <haukong1308@gmail.com>
2026-08-11 15:32:51 +0700

    Add new Table

---
+++ Ecommerce.Domain/Review.cs
@@ -0,0 +1,41 @@
+﻿using Ecommerce.Domain.Models;
+
+namespace Ecommerce.Domain
+{
+    public class Review
+    {
+        public int Id { get; private set; }
+
+        public int Rating { get; private set; }
+
+        public string Content { get; private set; } = string.Empty;
+
+        public string UserId { get; private set; } = string.Empty;
+        public int ProductId { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public User User { get; private set; } = null!;
+        public Product Product { get; private set; } = null!;
+
+        public Review() { }
+        public static Review Create(
+        string userId,
+        int productId,
+        int rating,
+        string content)
+        {
+            if (rating < 1 || rating > 5)
+                throw new ArgumentOutOfRangeException(nameof(rating));
+
+            return new Review
+            {
+                UserId = userId,
+                ProductId = productId,
+                Rating = rating,
+                Content = content,
+                CreatedAt = DateTime.UtcNow
+            };
+        }
+    }
+}
