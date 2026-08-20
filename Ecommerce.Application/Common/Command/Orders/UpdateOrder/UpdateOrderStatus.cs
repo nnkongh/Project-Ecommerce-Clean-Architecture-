8eb0d5027810c5bccf497f55a5f5341327724769
NguyenHau <haukong1308@gmail.com>
2026-08-20 23:46:39 +0700

    feat: Create Shop Handler

---
+++ Ecommerce.Application/Common/Command/Orders/UpdateOrder/UpdateOrderStatus.cs
@@ -0,0 +1,56 @@
+﻿using Ecommerce.Application.Interfaces;
+using Ecommerce.Domain;
+using Ecommerce.Domain.Interfaces;
+using Ecommerce.Domain.Interfaces.UnitOfWork;
+using Ecommerce.Domain.Shared;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Common.Command.Orders.UpdateOrder
+{
+    public sealed record UpdateOrderStatus(string UserId, int OrderId) : IRequest<Result>
+    {
+    }
+    internal sealed class UpdateOrderStatusHandler : IRequestHandler<UpdateOrderStatus, Result>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly INotificationService _notification;
+
+        public UpdateOrderStatusHandler(IOrderRepository orderRepository, IUnitOfWork unitOfWork, INotificationService notification)
+        {
+            _orderRepository = orderRepository;
+            _unitOfWork = unitOfWork;
+            _notification = notification;
+        }
+
+        public async Task<Result> Handle(UpdateOrderStatus request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetOrderByIdAsync(request.OrderId);
+            if (order == null)
+            {
+                return Result.Failure(new Error("404", "Không tìm thấy sản phẩm"));
+            }
+            if (order.OrderStatus != Domain.Enum.OrderStatus.Pending)
+            {
+                return Result.Failure(new Error("400", "Chỉ có thể cập nhật sản phẩm với trạng thái pending"));
+            }
+            if (order.CustomerId != request.UserId)
+            {
+                return Result.Failure(new Error("403", "Không thể xác nhận sản phẩm của người khác"));
+            }
+            order.UpdateStatus(Domain.Enum.OrderStatus.Processing);
+            await _orderRepository.Update(order);
+
+            var noti = Notification.Create("Xác nhận đơn", "Đơn hàng của bạn đã được cửa hàng xác nhận",order.CustomerId);
+            await _notification.SendNotificationAsync(order.CustomerId, noti);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+            
+            return Result.Success();
+        }
+    }
+}
---
+++ Ecommerce.Application/Common/Command/Shops/CreateShopCommand.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Ecommerce.Application.DTOs.Models;
+using Ecommerce.Domain;
+using Ecommerce.Domain.Interfaces;
+using Ecommerce.Domain.Interfaces.UnitOfWork;
+using Ecommerce.Domain.Shared;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Common.Command.Shops
+{
+    public sealed record CreateShopCommand(string UserId, string ShopName) : IRequest<Result<ShopModel>>
+    {
+    }
+
+
+
+    internal sealed class CreateShopHandler : IRequestHandler<CreateShopCommand, Result<ShopModel>>
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IShopRepository _shopRepository;
+        private readonly IMapper _mapper;
+
+        public CreateShopHandler(IShopRepository shopRepository, IUnitOfWork uow, IMapper mapper)
+        {
+            _shopRepository = shopRepository;
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<ShopModel>> Handle(CreateShopCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _shopRepository.GetByUserIdAsync(request.UserId);
+            if (user != null)
+            {
+                return Result.Failure<ShopModel>(new Error("400", "Một người chỉ được sở hữu một cửa hàng"));
+            }
+
+            var shop = Shop.Create(request.ShopName, request.UserId);
+
+            await _shopRepository.AddAsync(shop);
+            await _uow.SaveChangesAsync(cancellationToken);
+
+            var model = _mapper.Map<ShopModel>(shop); 
+            return Result.Success(model);
+
+        }
+    }
+
+    public class ShopModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public AddressModel Address { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt {get;set;}
+        public IReadOnlyList<ProductModel> ProductModels { get; set; }
+        public string ShopName { get; set; }
+    }
+}
