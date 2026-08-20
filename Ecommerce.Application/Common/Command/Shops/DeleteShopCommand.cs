c9ca34f18b49e43cc308334e8a83a073b0d658a9
NguyenHau <haukong1308@gmail.com>
2026-08-21 00:02:38 +0700

    feat: Add Delete shop handler

---
+++ Ecommerce.Application/Common/Command/Shops/DeleteShopCommand.cs
@@ -0,0 +1,45 @@
+﻿using Ecommerce.Domain.Interfaces;
+using Ecommerce.Domain.Interfaces.UnitOfWork;
+using Ecommerce.Domain.Shared;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Common.Command.Shops
+{
+    public sealed record DeleteShopCommand(string UserId, int ShopId) : IRequest<Result>
+    {
+    }
+    public sealed class DeleteShopHandler : IRequestHandler<DeleteShopCommand, Result>
+    {
+        private readonly IShopRepository _shopRepository;
+        private readonly IUnitOfWork _uow;
+
+        public DeleteShopHandler(IUnitOfWork uow, IShopRepository shopRepository)
+        {
+            _uow = uow;
+            _shopRepository = shopRepository;
+        }
+
+        public async Task<Result> Handle(DeleteShopCommand request, CancellationToken cancellationToken)
+        {
+            var shop = await _shopRepository.GetByIdAsync(request.ShopId);
+            if (shop == null)
+            {
+                return Result.Failure(new Error("404", "Không tìm thấy cửa hàng nào"));
+            }
+            if(shop.UserId != request.UserId)
+            {
+                return Result.Failure(new Error("403", "Đây không phải cửa hàng của bạn"));
+            }
+
+            await _shopRepository.Delete(shop);
+            await _uow.SaveChangesAsync(cancellationToken);
+
+            return Result.Success(model);
+        }
+    }
+}
