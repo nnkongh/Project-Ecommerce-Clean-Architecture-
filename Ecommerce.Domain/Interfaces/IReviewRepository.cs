ac12b87894064e995f50c70b3307648c0b4df8e7
NguyenHau <haukong1308@gmail.com>
2026-08-21 14:29:50 +0700

    featL add review interface

--- Ecommerce.Domain/Interfaces/IReviewRepository.cs
+++ Ecommerce.Domain/Interfaces/IReviewRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Ecommerce.Domain.Interfaces.Base;
+using Ecommerce.Domain.Models;
 
 namespace Ecommerce.Domain.Interfaces
 {
     public interface IReviewRepository : IRepositoryBase<Review, int>
     {
+        Task<Review?> GetByIdWithUserAsync(int id);
     }
 }
