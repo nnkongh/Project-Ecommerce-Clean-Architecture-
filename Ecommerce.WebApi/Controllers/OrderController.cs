28515ef608313d9cbbc8ad37d3a317fa9c3d1283
NguyenHau <haukong1308@gmail.com>
2026-08-22 14:57:56 +0700

    feat: add createorder and use authoeize prop

--- Ecommerce.WebApi/Controllers/OrderController.cs
+++ Ecommerce.WebApi/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.Application.Common.Command.Orders.CreateOrder;
+using Ecommerce.Application.Common.Command.Orders.UpdateOrder;
 using Ecommerce.Application.Common.Queries.Orders;
 using Ecommerce.Application.Common.Queries.Orders.GetListOrderByUserId;
 using Ecommerce.Application.Common.Queries.Orders.GetOrderById;
@@ -9,12 +10,13 @@ using Ecommerce.WebApi.Controllers.BaseController;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
 using System.Security.Claims;
 
 namespace Ecommerce.WebApi.Controllers
 {
     [Authorize]
-    [Route("orders")]
+    [Route("order")]
     public class OrderController : ApiController
     {
         public OrderController(ISender sender) : base(sender)
@@ -42,19 +44,33 @@ namespace Ecommerce.WebApi.Controllers
                                     : BadRequest(new ApiResponse<OrderModel> { IsSuccess = false, Error = result.Error });
         }
         [HttpPost]
-        public async Task<IActionResult> CreateOrder(CreateOrderRequest order)
+        [Authorize]
+        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderApiRequest request)
         {
-            //var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            //if (userId == null)
-            //{
-            //    return Unauthorized();
-            //}
-            //var command = new CreateOrderCommand(order,userId);
-            //var result = await Sender.Send(command);
-            //return result.IsSuccess ? Ok(new ApiResponse<OrderModel> { IsSuccess = true, Value = result.Value })
-            //                        : BadRequest(new ApiResponse<OrderModel> { IsSuccess = false, Error = result.Error });
-            return Ok();
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+            var command = new CreateOrderCommand(request.ProductId, userId, request.Quantity);
+            var result = await Sender.Send(command);
+            return result.IsSuccess ? Ok(new ApiResponse<OrderModel> { IsSuccess = true, Value = result.Value })
+                                    : BadRequest(new ApiResponse<OrderModel> { IsSuccess = false, Error = result.Error });
         }
-        
+        [HttpPut("{orderId}")]
+        public async Task<IActionResult> UpdateOrderStatus(int orderId) {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+            var handler = new UpdateOrderStatus(userId, orderId);
+            var result = await Sender.Send(handler);
+
+            return result.IsSuccess ? Ok(new ApiResponse<OrderModel> { IsSuccess = true})
+                                    : BadRequest(new ApiResponse<OrderModel> { IsSuccess = false, Error = result.Error });
+        }
     }
+
+    public record CreateOrderApiRequest(int ProductId, int Quantity);
 }
