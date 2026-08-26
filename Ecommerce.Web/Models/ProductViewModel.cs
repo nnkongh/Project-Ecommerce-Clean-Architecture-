e01e2ddac587a44e08ca3a102f61d6011482bbfa
NguyenHau <haukong1308@gmail.com>
2026-08-26 22:24:10 +0700

    fix: update childcategoryid into view and view model

--- Ecommerce.Web/Models/ProductViewModel.cs
+++ Ecommerce.Web/Models/ProductViewModel.cs
@@ -12,7 +12,8 @@ namespace Ecommerce.Web.ViewModels
         public string? ImageUrl { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
-        public int? CategoryId { get; set; }
+        public int? ParentCategoryId { get; set; }
+        public int? ChildCategoryId { get; set; }
         public List<SelectListItem> ParentCategories { get; set; } = new();
         public List<CategoryViewModel>? AllCategories { get; set; } = new();
     }
