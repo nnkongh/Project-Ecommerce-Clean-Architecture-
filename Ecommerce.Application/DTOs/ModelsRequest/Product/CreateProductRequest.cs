300b557b98cc98008efc2d4bfdfcdbf73e857f27
NguyenHau <haukong1308@gmail.com>
2026-08-28 23:23:02 +0700

    chore: nullable description

--- Ecommerce.Application/DTOs/ModelsRequest/Product/CreateProductRequest.cs
+++ Ecommerce.Application/DTOs/ModelsRequest/Product/CreateProductRequest.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ecommerce.Application.DTOs.ModelsRequest.Product
+﻿namespace Ecommerce.Application.DTOs.ModelsRequest.Product
 {
-    public record CreateProductRequest(string Name, string Description, string ImageUrl, decimal Price, int Stock, int CategoryId)
+    public record CreateProductRequest(string Name, string? Description, string? ImageUrl, decimal Price, int Stock, int ParentCategoryId, int ChildCategoryId)
     {
     }
 }
