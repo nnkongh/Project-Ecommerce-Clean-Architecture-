edd7b844fcfbd75960e4928410bd4c2830d30414
NguyenHau <haukong1308@gmail.com>
2026-08-28 22:27:13 +0700

    chore: update migrations

---
+++ Ecommerce.Infrastructure/Migrations/ApplicationDb/20260828144237_ChangeProp.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations.ApplicationDb
+{
+    /// <inheritdoc />
+    public partial class ChangeProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql("UPDATE Products SET ParentCategoryId = CategoryId " + "WHERE ParentCategoryId = 0 AND CategoryId > 0");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Products");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ParentCategoryId",
+                table: "Products",
+                column: "ParentCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_ParentCategoryId",
+                table: "Products",
+                column: "ParentCategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_ParentCategoryId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ParentCategoryId",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
