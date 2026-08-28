9e44bd775a02e0170294a2162bc5bb666daf0cb2
NguyenHau <haukong1308@gmail.com>
2026-08-28 23:15:33 +0700

    chore: nullable description

--- Ecommerce.Web/Models/ProductViewModel.cs
+++ Ecommerce.Web/Models/ProductViewModel.cs
@@ -7,7 +7,7 @@ namespace Ecommerce.Web.ViewModels
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public string? Description { get; set; }
+        public string? Description { get; set; } = null!;
         public IFormFile? Image { get; set; } = null!;
         public string? ImageUrl { get; set; }
         public decimal Price { get; set; }
