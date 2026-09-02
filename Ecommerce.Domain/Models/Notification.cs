8aa7580c3a265b8041d2e9f3af669fac377a6669
NguyenHau <haukong1308@gmail.com>
2026-09-02 23:38:53 +0700

    chore: update isread and createdate prop

--- Ecommerce.Domain/Models/Notification.cs
+++ Ecommerce.Domain/Models/Notification.cs
@@ -12,18 +12,27 @@ namespace Ecommerce.Domain.Models
         public string Title { get; private set; } = string.Empty;
         public string Description { get; private set; } = string.Empty;
         public string UserId { get; private set; } = string.Empty;
+        public bool IsRead { get; private set; }
+        public DateTime CreatedDate { get; private set; }
         public User User { get; private set; }
         public Notification() { }
 
-        public static Notification Create(string title, string description,string userId)
+        public static Notification Create(string title, string description, string userId)
         {
             var notification = new Notification
             {
                 Description = description,
                 Title = title,
-                UserId = userId
+                UserId = userId,
+                IsRead = false,
+                CreatedDate = DateTime.Now
             };
             return notification;
+        }
+
+        public void MarkAsRead()
+        {
+            IsRead = true;
         }
     }
 }
