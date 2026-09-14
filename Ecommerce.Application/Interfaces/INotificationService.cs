f6055f9d4d092e89150daf6b5bda802338b6324d
NguyenHau <haukong1308@gmail.com>
2026-09-14 22:35:34 +0700

    chore: create send notification interface

---
+++ Ecommerce.Application/Interfaces/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Interfaces
+{
+    public interface INotificationService
+    {
+        Task SendNotificationAsync(Notification noti, CancellationToken cancellationToken);
+    }
+}
---
+++ Ecommerce.Infrastructure/Interfaces/INotificationHubService.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Interfaces
+{
+    public interface INotificationHubService
+    {
+        Task SendNotificationToUserAsync(string id, object message);
+
+    }
+}
