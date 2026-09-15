6a41bed813dae35e06ffe6a0429f395031839eb6
NguyenHau <haukong1308@gmail.com>
2026-09-15 22:58:01 +0700

    fix: rename map chathub

--- Ecommerce.WebApi/Program.cs
+++ Ecommerce.WebApi/Program.cs
@@ -6,6 +6,7 @@ using Ecommerce.Infrastructure;
 using Ecommerce.Infrastructure.Data;
 using Ecommerce.Infrastructure.Dependency_Injection;
 using Ecommerce.Infrastructure.Mapper;
+using Ecommerce.Infrastructure.Services;
 using Ecommerce.Infrastructure.Services.ExternalAuth;
 using Ecommerce.WebApi.Dependencies;
 using Ecommerce.WebApi.Services;
@@ -48,6 +49,7 @@ namespace Ecommerce.WebApi
             app.UseCors("SignalRPolicy");
             app.UseAuthentication();
             app.UseAuthorization();
+            app.MapHub<ChatHub>("/chatHub");
 
             app.MapControllers();
 
