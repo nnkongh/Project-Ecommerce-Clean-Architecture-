4ccf6e1906843012ffe954679c7815d7b9aaaf9e
NguyenHau <haukong1308@gmail.com>
2026-09-16 22:07:09 +0700

    chore: update order id in notification entity

---
+++ Ecommerce.Infrastructure/Migrations/ApplicationDb/20260916144529_AddNotificationOrderId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations.ApplicationDb
+{
+    /// <inheritdoc />
+    public partial class AddNotificationOrderId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Notifications",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Notifications");
+        }
+    }
+}
