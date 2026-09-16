d4372ec07f3ff978a03ef7387e32b40df55c0e01
NguyenHau <haukong1308@gmail.com>
2026-09-16 22:10:11 +0700

    chore: add noti view model web

--- Ecommerce.Web/Models/NotificationViewModel.cs
+++ Ecommerce.Web/Models/NotificationViewModel.cs
@@ -5,6 +5,7 @@ namespace Ecommerce.Web.ViewModels
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public int? OrderId { get; set; }
         public bool IsRead { get; set; }
         public DateTime CreatedDate { get; set; }
     }
