ebb7231f59a51ddc51f663bacc661bb021d54602
NguyenHau <haukong1308@gmail.com>
2026-09-16 22:11:27 +0700

    chore: add orderid prop notification

--- Ecommerce.Domain/Models/Notification.cs
+++ Ecommerce.Domain/Models/Notification.cs
@@ -12,18 +12,20 @@ namespace Ecommerce.Domain.Models
         public string Title { get; private set; } = string.Empty;
         public string Description { get; private set; } = string.Empty;
         public string UserId { get; private set; } = string.Empty;
+        public int? OrderId { get; private set; }
         public bool IsRead { get; private set; }
         public DateTime CreatedDate { get; private set; }
         public User User { get; private set; }
         public Notification() { }
 
-        public static Notification Create(string title, string description, string userId)
+        public static Notification Create(string title, string description, string userId, int? orderId = null)
         {
             var notification = new Notification
             {
                 Description = description,
                 Title = title,
                 UserId = userId,
+                OrderId = orderId,
                 IsRead = false,
                 CreatedDate = DateTime.Now
             };
