7ef1af72f3908326a45c80e528509ce096d16c2a
NguyenHau <haukong1308@gmail.com>
2026-09-19 23:34:41 +0700

    feat: create interface vnpay payment

---
+++ Ecommerce.Infrastructure/Interfaces/IPaymentClient.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VNPAY.Models;
+using VNPAY.Models.Enums;
+
+namespace Ecommerce.Infrastructure.Interfaces
+{
+    public interface IPaymentClient
+    {
+        PaymentUrlDetail CreatePaymentUrl(VnpayPaymentRequest request);
+
+        
+        PaymentUrlDetail CreatePaymentUrl(double money, string description, BankCode bankCode = BankCode.ANY);
+
+
+        VnpayPaymentResult GetPaymentResult(IQueryCollection parameters);
+
+
+        VnpayPaymentResult GetPaymentResult(HttpRequest httpRequest);
+    }
+}
